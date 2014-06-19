2e6803cfc8c5f5fa7db8f94cb157b3aef1645112
Jason L. Walker <jasonlanewalker@gmail.com>
2014-06-18 20:33:35 -0500

    starting flashy exercise

---
+++ Flashy/Flashy/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flashy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int c = 0; c < 253; c++)
+            {
+                this.BackColor = Color.FromArgb(c, 255 - c, c);
+                Application.DoEvents();
+            }
+        }
+    }
+}
