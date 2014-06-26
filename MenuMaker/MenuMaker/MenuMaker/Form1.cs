293fcd1e94e1b5266fdb90337d0335b5eddf32d0
Jason L. Walker <jasonlanewalker@gmail.com>
2014-06-25 23:14:57 -0500

    Completed MenuMaker exercise

---
+++ MenuMaker/MenuMaker/MenuMaker/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MenuMaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            MenuMaker menu = new MenuMaker() { Randomizer = new Random() };
+
+            label1.Text = menu.MakeSandwich();
+            label2.Text = menu.MakeSandwich();
+            label3.Text = menu.MakeSandwich();
+            label4.Text = menu.MakeSandwich();
+            label5.Text = menu.MakeSandwich();
+            label6.Text = menu.MakeSandwich();
+        }
+    }
+}
---
+++ MenuMaker/MenuMaker/MenuMaker/MenuMaker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuMaker
+{
+    class MenuMaker
+    {
+
+        string[] Meats = { "Roast Beef", "Salami", "Turkey", "Chicken", "Bologna", "Pepperoni" };
+        string[] Condiments = { "Mayo", "Ketchup", "Mustard", "Pickles", "Onions", "Horse radish" };
+        string[] Breads = { "White", "Wheat", "Rye", "Italian", "Honey Oats" };
+        public Random Randomizer;
+
+        public string MakeSandwich()
+        {
+            string meat = Meats[Randomizer.Next(Meats.Length)];
+            string condiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string bread = Breads[Randomizer.Next(Breads.Length)];
+            return "Made a " + meat + " sandwich with " + condiment + " on " + bread + " bread.";
+        }
+    }
+}
