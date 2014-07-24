098b6f9cb2eb94c954000937f02b1abad564dcf6
Jason L. Walker <jasonlanewalker@gmail.com>
2014-07-24 15:52:43 -0500

    Started house exercise

---
+++ House/House/House/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace House
+{
+    abstract class Location
+    {
+        public string Name { get; set; }
+        public Location[] Exits { get; set; }
+        public string Description { get; set; }
+
+    }
+}
