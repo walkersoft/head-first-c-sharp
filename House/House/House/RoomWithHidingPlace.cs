1d38404b82fe959c9b6b57a187d3b4ca4611264a
Jason L. Walker <jasonlanewalker@gmail.com>
2014-08-01 05:19:56 -0500

    Adding untracked files for house exercise

---
+++ House/House/House/RoomWithHidingPlace.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace House
+{
+    public class RoomWithHidingPlace : Room, IHidingPlace
+    {
+        private string hidingPlace;
+
+        public RoomWithHidingPlace(string name, string decoration, string hidingPlace) : base(name, decoration) 
+        {
+            this.hidingPlace = hidingPlace;
+        }
+
+        public string HidingPlace
+        {
+            get { return hidingPlace; }
+        }
+    }
+}
