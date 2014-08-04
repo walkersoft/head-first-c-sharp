4bbed60a4aa41083c34321098baa52a59a814433
Jason L. Walker <jasonlanewalker@gmail.com>
2014-08-04 15:06:18 -0500

    Finished Duck exercise

---
+++ DuckCollection/DuckCollection/DuckCollection/Duck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuckCollection
+{
+    class Duck : IComparable<Duck>
+    {
+        public int Size;
+        public KindOfDuck Kind;
+
+        public int CompareTo(Duck compareTo)
+        {
+            if (this.Size > compareTo.Size)
+                return 1;
+            else if (this.Size < compareTo.Size)
+                return -1;
+            else
+                return 0;
+        }
+    }
+}
---
+++ DuckCollection/DuckCollection/DuckCollection/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuckCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Duck> ducks = new List<Duck>() 
+            {
+                new Duck() { Kind = KindOfDuck.Mallard, Size = 17 },
+                new Duck() { Kind = KindOfDuck.Muscovy, Size = 18 },
+                new Duck() { Kind = KindOfDuck.Decoy, Size = 14 },
+                new Duck() { Kind = KindOfDuck.Muscovy, Size = 11 },
+                new Duck() { Kind = KindOfDuck.Mallard, Size = 14 },
+                new Duck() { Kind = KindOfDuck.Decoy, Size = 13 }
+            };
+
+            DuckComparerBySize duckSizeCompare = new DuckComparerBySize();
+            DuckComparerByKind duckKindCompare = new DuckComparerByKind();
+
+            PrintDucks(ducks);
+            ducks.Sort(duckSizeCompare);
+            PrintDucks(ducks);
+            ducks.Sort(duckKindCompare);
+            PrintDucks(ducks);
+
+            Console.ReadKey();
+        }
+
+        public static void PrintDucks(List<Duck> ducks)
+        {
+            foreach (Duck duck in ducks)
+            {
+                Console.WriteLine(duck.Size.ToString() + "-inch " + duck.Kind.ToString());
+            }
+            Console.WriteLine("End of Ducks!");
+        }
+    }
+}
