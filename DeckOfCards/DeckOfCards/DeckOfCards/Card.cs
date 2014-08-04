3448c433f48afc55fa1afe89581e42a3d4b18035
Jason L. Walker <jasonlanewalker@gmail.com>
2014-08-04 16:07:39 -0500

    Complete Random Cards exercise

---
+++ DeckOfCards/DeckOfCards/DeckOfCards/Card.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckOfCards
+{
+    class Card : IComparable<Card>
+    {
+        public Suit Suit;
+        public Value Value;
+
+        public int CompareTo(Card other)
+        {
+            if (this.Value < other.Value)
+                return -1;
+            else if (this.Value > other.Value)
+                return 1;
+            else
+                return 0;
+        }
+
+        public Card(Suit suit, Value value)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+    }
+}
---
+++ DeckOfCards/DeckOfCards/DeckOfCards/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Card> cards = new List<Card>();
+            CardSorter cardSorter = new CardSorter();
+
+
+            while (Console.OpenStandardInput() != null)
+            {
+                MakeFiveCards(cards);
+                PrintCards(cards);
+                cards.Sort(cardSorter);
+                PrintCards(cards);
+                Console.ReadKey();
+            }
+        }
+
+        public static void MakeFiveCards(List<Card> list)
+        {
+            list.Clear();
+            Random random = new Random();
+            for (int i = 0; i < 5; i++) 
+            { 
+                Card card = new Card((Suit)random.Next(4), (Value)random.Next(1, 14));
+                list.Add(card);
+            }
+        }
+
+        public static void PrintCards(List<Card> cards)
+        {
+            foreach (Card card in cards)
+            {
+                Console.WriteLine(card.Value + " of " + card.Suit);
+            }
+            Console.WriteLine("There's your hand!");
+            Console.WriteLine("");
+        }
+    }
+}
