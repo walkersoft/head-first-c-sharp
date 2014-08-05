277e4daec62d842fb68432d9a5b6a0d1d1257780
Jason L. Walker <jasonlanewalker@gmail.com>
2014-08-04 22:36:24 -0500

    Working on Duck/Bird exercise

---
+++ DuckCollection/DuckEnumeration/DuckEnumerator/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuckEnumerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Duck> ducks = new List<Duck>() 
+            {
+                new Duck() { Kind = KindOfDuck.Mallard, Size = 17 },
+                new Duck() { Kind = KindOfDuck.Muscovy, Size = 18 },
+                new Duck() { Kind = KindOfDuck.Decoy, Size = 14 },
+                new Duck() { Kind = KindOfDuck.Muscovy, Size = 11 },
+                new Duck() { Kind = KindOfDuck.Mallard, Size = 14 },
+                new Duck() { Kind = KindOfDuck.Decoy, Size = 13 }
+            };
+            IEnumerable<Bird> upcastDucks = ducks;
+
+            List<Bird> birds = new List<Bird>();
+            birds.Add(new Bird() { Name = "Feathers" });
+            birds.AddRange(upcastDucks);
+            birds.Add(new Penguin() { Name = "George" });
+
+            foreach (Bird bird in birds)
+            {
+                Console.WriteLine(bird);
+            }
+
+            Console.ReadKey();
+        }
+    }
+    enum KindOfDuck
+    {
+        Mallard,
+        Muscovy,
+        Decoy,
+    }
+
+
+    public class Bird
+    {
+        public string Name { get; set; }
+
+        public virtual void Fly()
+        {
+            Console.WriteLine("Flap, flap");
+        }
+
+        public override string ToString()
+        {
+            return "A bird named " + Name;
+        }
+    }
+
+    public class Penguin : Bird
+    {
+        public override void Fly()
+        {
+            Console.WriteLine("Penguins can't fly");
+        }
+
+        public override string ToString()
+        {
+            return "A bird named " + Name;
+        }
+    }
+
+
+
+    class Duck : Bird, IComparable<Duck>
+    {
+        public int Size;
+        public KindOfDuck Kind;
+
+        public int CompareTo(Duck compareTo)
+        {
+            if (this.Size > compareTo.Size)
+                return 1;
+            else if (this.Size < compareTo.Size)
+                return -1;
+            else
+                return 0;
+        }
+
+        public override string ToString()
+        {
+            return Size + "-inch " + Kind;
+        }
+    }
+}
---
+++ DuckCollection/DuckEnumeration/DuckEnumerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DuckEnumerator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DuckEnumerator")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("852dd2e8-1382-4706-8a0f-f4e8b3f28925")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
