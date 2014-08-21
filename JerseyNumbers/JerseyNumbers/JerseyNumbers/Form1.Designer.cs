032ab0e467e6403f0fc271f04efa673300341d12
Jason L. Walker <jasonlanewalker@gmail.com>
2014-08-21 14:22:19 -0500

    Did dictionary collection exercise

---
+++ JerseyNumbers/JerseyNumbers/JerseyNumbers/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace JerseyNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.number = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.year = new System.Windows.Forms.TextBox();
+            this.player = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // number
+            // 
+            this.number.FormattingEnabled = true;
+            this.number.Location = new System.Drawing.Point(62, 12);
+            this.number.Name = "number";
+            this.number.Size = new System.Drawing.Size(50, 21);
+            this.number.TabIndex = 0;
+            this.number.SelectedIndexChanged += new System.EventHandler(this.number_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "was worn by";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(313, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "and retired in";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Number";
+            // 
+            // year
+            // 
+            this.year.Location = new System.Drawing.Point(387, 12);
+            this.year.Name = "year";
+            this.year.Size = new System.Drawing.Size(44, 20);
+            this.year.TabIndex = 4;
+            // 
+            // player
+            // 
+            this.player.Location = new System.Drawing.Point(185, 12);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(122, 20);
+            this.player.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 46);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.year);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.number);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox number;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox year;
+        private System.Windows.Forms.TextBox player;
+    }
+}
+
