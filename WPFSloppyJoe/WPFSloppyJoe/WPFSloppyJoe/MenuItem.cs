f946252360454bc6cb23291f8e0a974d076ab127
Jason L. Walker <jasonlanewalker@gmail.com>
2015-01-13 00:29:41 -0600

    Head first into WPF

---
+++ WPFSloppyJoe/WPFSloppyJoe/WPFSloppyJoe/MenuItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFSloppyJoe
+{
+    class MenuItem
+    {
+        public string Meat { get; set; }
+        public string Condiment { get; set; }
+        public string Bread { get; set; }
+
+        public MenuItem(string meat, string condiment, string bread)
+        {
+            this.Meat = meat;
+            this.Condiment = condiment;
+            this.Bread = bread;
+        }
+
+        public override string ToString()
+        {
+            return Meat + " with " + Condiment + " on " + Bread;
+        }
+    }
+}
---
+++ WPFSloppyJoe/WPFSloppyJoe/WPFSloppyJoe/MenuMaker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace WPFSloppyJoe
+{
+    class MenuMaker
+    {
+        //Sandwich goodies
+        private List<String> meats = new List<String>()
+        {
+            "Roast beef", "Turkey", "Salami", "Honey ham", "Pastrami", "Bologna"
+        };
+
+        private List<String> breads = new List<String>()
+        {
+            "white", "wheat", "rye", "honey oats", "italian", "flatbread", "sour dough"
+        };
+
+        private List<String> condiments = new List<String>()
+        {
+            "onion", "mayo", "mustard", "pickle", "olives", "lettuce", "tomato", "cheese"
+        };
+
+        private Random random = new Random();
+
+        public ObservableCollection<MenuItem> Menu { get; private set; }
+        public DateTime GeneratedDate { get; set; }
+        public int NumberOfItems { get; set; }
+
+        public MenuMaker()
+        {
+            this.Menu = new ObservableCollection<MenuItem>();
+            NumberOfItems = 10;
+            UpdateMenu();
+        }
+
+        public void UpdateMenu()
+        {
+            Menu.Clear();
+            for (int i = 0; i < NumberOfItems; i++)
+            {
+                Menu.Add(CreateMenuItem());
+            }
+            GeneratedDate = DateTime.Now;
+        }
+
+        public MenuItem CreateMenuItem()
+        {
+            return new MenuItem(meats[random.Next(meats.Count)], condiments[random.Next(condiments.Count)], breads[random.Next(breads.Count)]);
+        }
+    }
+}
